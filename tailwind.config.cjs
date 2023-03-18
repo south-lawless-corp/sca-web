6dedbc5f6d3275df1679f1980f12baf452949dd9
lia-07 <107384739+lia-07@users.noreply.github.com>
2023-03-19 11:44:34 +1300

    feat: :lipstick: add slc custom colours to TailwindCSS

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,16 @@ const config = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 
 	theme: {
-		extend: {}
+		extend: {
+			colors: {
+				'slc-dark': '#1C1C1F',
+				'slc-darkish': '#545454',
+				'slc-lightish': '#b5b5b5',
+				'slc-light': '#e9e9e9',
+
+				'slc-blue': '#3D44A4'
+			}
+		}
 	},
 
 	plugins: []
